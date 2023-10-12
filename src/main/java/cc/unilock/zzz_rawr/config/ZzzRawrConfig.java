0ebeb1d351852cda9aec282b0b7256d4d62ba1a0
unilock <unilock@fennet.rentals>
2023-10-12 08:19:02 -0400

    Remove unnecessary entrypoint by making config self-instantiating
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/zzz_rawr/config/ZzzRawrConfig.java
+++ src/main/java/cc/unilock/zzz_rawr/config/ZzzRawrConfig.java
@@ -4,8 +4,11 @@ import org.quiltmc.config.api.ReflectiveConfig;
 import org.quiltmc.config.api.annotations.Comment;
 import org.quiltmc.config.api.values.TrackedValue;
 import org.quiltmc.config.api.values.ValueList;
+import org.quiltmc.loader.api.config.v2.QuiltConfig;
 
 public class ZzzRawrConfig extends ReflectiveConfig {
+	public static final ZzzRawrConfig INSTANCE = QuiltConfig.create("zzz_rawr", "config", ZzzRawrConfig.class);
+
 	@Comment("List of messages to send (one will be chosen at random)")
 	public final TrackedValue<ValueList<String>> messages = this.list("",
 			"Wakey, wakey, rise and shine... Good Morning everyone!",
--- src/main/java/cc/unilock/zzz_rawr/mixin/ServerWorldMixin.java
+++ src/main/java/cc/unilock/zzz_rawr/mixin/ServerWorldMixin.java
@@ -1,6 +1,6 @@
 package cc.unilock.zzz_rawr.mixin;
 
-import cc.unilock.zzz_rawr.ZzzRawr;
+import cc.unilock.zzz_rawr.config.ZzzRawrConfig;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -15,6 +15,6 @@ import java.util.function.BooleanSupplier;
 public class ServerWorldMixin {
 	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;wakeSleepingPlayers()V"))
 	public void onWakeSleepingPlayers(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-		((ServerWorld) (Object) this).getServer().getPlayerManager().broadcastSystemMessage(Text.literal(ZzzRawr.CONFIG.messages.value().get(((ServerWorld) (Object) this).getRandom().nextInt(ZzzRawr.CONFIG.messages.value().size()))).formatted(Formatting.GOLD), false);
+		((ServerWorld) (Object) this).getServer().getPlayerManager().broadcastSystemMessage(Text.literal(ZzzRawrConfig.INSTANCE.messages.value().get(((ServerWorld) (Object) this).getRandom().nextInt(ZzzRawrConfig.INSTANCE.messages.value().size()))).formatted(Formatting.GOLD), false);
 	}
 }
