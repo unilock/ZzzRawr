f459a99a618932ece504e0ec1049d9da9cdf7ebe
unilock <unilock@fennet.rentals>
2024-01-16 16:19:09 -0500

    v2.0.0: Port to Fabric + Kaleido
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/zzz_rawr/config/ZzzRawrConfig.java
+++ src/main/java/cc/unilock/zzz_rawr/config/ZzzRawrConfig.java
@@ -1,16 +1,15 @@
 package cc.unilock.zzz_rawr.config;
 
-import org.quiltmc.config.api.ReflectiveConfig;
-import org.quiltmc.config.api.annotations.Comment;
-import org.quiltmc.config.api.values.TrackedValue;
-import org.quiltmc.config.api.values.ValueList;
-import org.quiltmc.loader.api.config.v2.QuiltConfig;
+import folk.sisby.kaleido.api.WrappedConfig;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
+import folk.sisby.kaleido.lib.quiltconfig.api.values.ValueList;
+import net.fabricmc.loader.api.FabricLoader;
 
-public class ZzzRawrConfig extends ReflectiveConfig {
-	public static final ZzzRawrConfig INSTANCE = QuiltConfig.create("zzz_rawr", "config", ZzzRawrConfig.class);
+public class ZzzRawrConfig extends WrappedConfig {
+	public static final ZzzRawrConfig INSTANCE = ZzzRawrConfig.createToml(FabricLoader.getInstance().getConfigDir(), "zzz_rawr", "config", ZzzRawrConfig.class);
 
 	@Comment("List of messages to send (one will be chosen at random)")
-	public final TrackedValue<ValueList<String>> messages = this.list("",
+	public final ValueList<String> messages = ValueList.create("",
 			"Wakey, wakey, rise and shine... Good Morning everyone!",
 			"zzz... rrr... Rawr?"
 	);
